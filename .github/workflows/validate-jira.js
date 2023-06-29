1c136533e18fff9aa8df8bfc380110b36778712d
Logan Davis <134395604+lwd658@users.noreply.github.com>
2023-06-29 11:48:15 -0400

    Update validate-jira.js

--- .github/workflows/validate-jira.js
+++ .github/workflows/validate-jira.js
@@ -12,7 +12,7 @@ function run() {
       );
     }
 
-    else if (!body.includes("atlassian")) {
+    else if (!body.includes("atlassian") && !body.includes("MMTAM") && !body.includes("MMTABCS")) {
       core.setFailed(
         "Please add a jira issue to the pull request description"
       );
